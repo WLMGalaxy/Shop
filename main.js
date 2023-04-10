bbb2e426d4a2df42578ed34d15b96491e6182602
idlers <778826356@qq.com>
2023-04-10 17:41:21 +0800

    完成了首页

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import {
+	$http
+} from '@escook/request-miniprogram'
+
+// 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+
+// 请求接口根路径
+$http.baseUrl = 'https://www.fastmock.site/mock/6c3454cb9716e486ccedb6060afe9551/shop'
+
+// 请求拦截器
+$http.beforeRequest = function(options) {
+	wx.showLoading({
+		title: '数据加载中...',
+	})
+}
+// 响应拦截器 就是响应完成之后做一些事
+$http.afterRequest = function() {
+	wx.hideLoading()
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 import App from './App.vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,38 @@
 <template>
 	<view>
-		
+		<!-- 轮播 -->
+		<swiper class="swiper-home" indicator-dots="true" autoplay="true" interval="2000" circular="true"
+			indicator-color="#000" indicator-active-color="#FF0000">
+			<swiper-item v-for="(item,index) in bannerList" :key="index">
+				<!-- <image :src="item.picUrl"></image> -->
+				<navigator :url="'/subpkg/goods-detail/goods-detail?goodid='+item.id">
+					<image :src="item.picUrl" mode=""></image>
+				</navigator>
+			</swiper-item>
+		</swiper>
+		<!-- 分类 -->
+		<view class="cates">
+			<view class="cate-item" v-for="(cateItem,cateIndex) in cateList" :key="cateIndex"
+				@click="goToCate(cateItem)">
+				<image :src="cateItem.catePic"></image>
+				<text>{{cateItem.cateName}}</text>
+			</view>
+		</view>
+		<!-- 楼层 -->
+		<view class="floor-list">
+			<view v-for="(floorItem,floorIndex) in floorList" :key="floorIndex" class="floor-item">
+				<view class="floor-title">{{floorItem.floor_title}}</view>
+				<view class="floor-con">
+					<view v-for="(floorSubItem,floorSubIndex) in floorItem.floor_con" class="sub-item">
+						<navigator :url="floorSubItem.url">
+							<image :src="floorSubItem.imgPic" mode=""></image>
+							<view class="proname">{{floorSubItem.name}}</view>
+							<view class="proprice">{{floorSubItem.price}}</view>
+						</navigator>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +40,166 @@
 	export default {
 		data() {
 			return {
-				
+				bannerList: [],
+				cateList: [],
+				floorList: [],
 			};
+		},
+		methods: {
+			// 获取Banner数据
+			async getBannerList() {
+				// 发送数据请求获取列表数据
+				// console.log(123)
+				const {
+					data: res
+				} = await uni.$http.get('/bannerlist')
+				// 判断是否请求成功
+				if (res.status === 200) {
+					// 将响应数据存到bannerList
+					this.bannerList = res.data.list
+				}
+			},
+			// 获取分类数据
+			async getCateList() {
+				const res = await uni.$http.get('/cates')
+				const {
+					data: {
+						cateList
+					},
+					status
+				} = res.data
+				if (status != 200) {
+					return uni.showToast({
+						title: '加载数据失败',
+						duration: 1000,
+						icon: 'none'
+					})
+				} else {
+					this.cateList = cateList
+				}
+			},
+			// 获取楼层数据
+			getFloorList: async function() {
+				const res = await uni.$http.get('/floors')
+				const {
+					data: {
+						floorList
+					},
+					status
+				} = res.data
+				if (status != 200) {
+					return uni.showToast({
+						title: '加载数据失败',
+						duration: 1000,
+						icon: 'none'
+					})
+				} else {
+					floorList.forEach((product) => {
+						product.floor_con.forEach(subProduct => {
+							subProduct.url = "/subpkg/goods-list/goods-list?name=" + subProduct.name
+						})
+					})
+					this.floorList = floorList
+				}
+			},
+			// 跳转到分类页面
+			goToCate: function(cateItem) {
+				if (cateItem.cateName == "分类") {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			},
+		},
+		onLoad() {
+			// 调用获取数据方法
+			this.getBannerList()
+			this.getCateList()
+			this.getFloorList()
 		}
 	}
 </script>
 
 <style lang="scss">
+	.swiper-home {
+		width: 100%;
+		background-color: #E43D33;
 
+		swiper-item {
+			width: 100%;
+
+			image {
+				width: 100%;
+				height: 300rpx;
+			}
+		}
+	}
+
+	.cates {
+		width: 100%;
+		height: 320rpx;
+		border-bottom: 1rpx solid #c7c7c7;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-around;
+		align-items: center;
+
+		.cate-item {
+			width: 20%;
+			height: 150rpx;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-around;
+			align-items: center;
+
+			image {
+				width: 78rpx;
+				height: 78rpx;
+			}
+
+			text {
+				font-size: 26rpx;
+			}
+		}
+	}
+
+	.floor-list {
+		width: 100%;
+
+		.floor-item {
+			width: 100%;
+			height: 840rpx;
+			background-color: #fff;
+
+			.floor-title {
+				height: 60rpx;
+				line-height: 60rpx;
+				font-weight: 900;
+			}
+
+			.floor-con {
+				width: 100%;
+				height: 400rpx;
+				display: flex;
+				flex-wrap: wrap;
+				justify-content: space-around;
+				align-items: center;
+
+				.sub-item {
+					width: 50%;
+					height: 400rpx;
+					display: flex;
+					flex-wrap: wrap;
+					flex-direction: column;
+					justify-content: space-around;
+					align-items: center;
+
+					image {
+						width: 220rpx;
+						height: 220rpx;
+					}
+				}
+			}
+		}
+	}
 </style>
